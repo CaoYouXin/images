da19373b521be42cb6ac512c12b555eb483e96c1
CaoYouXin <c_youxin@163.com>
2016-04-05 17:58:39 +0800

    hot fix

--- toonly/javascripts/init/index_.js
+++ toonly/javascripts/init/index_.js
@@ -11,9 +11,9 @@ if (window === window.top) {
         $(function () {
             window.top.require(['handlebars', 'toonly/javascripts/only-router'], function (Handlebars, router) {
 
-                $.getJSON('http://192.168.4.244:8080/myblog/articles.json', function (data) {
+                $.getJSON('/myblog/articles.json', function (data) {
 
-                    $.get('http://192.168.4.244:8080/myblog/toonly/x-handlebars-templates/article_list.html', function (tpl) {
+                    $.get('/myblog/toonly/x-handlebars-templates/article_list.html', function (tpl) {
                         var tplFn = Handlebars.compile(tpl);
                         var html = tplFn(data);
 
