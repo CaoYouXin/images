f2f7b53ef3f7a8a78ea8ea05c2af10806bf8c62e
CaoYouXin <c_youxin@163.com>
2016-04-06 23:00:49 +0800

    请使用谷歌浏览器打开

--- toonly/javascripts/init/index.js
+++ toonly/javascripts/init/index.js
@@ -3,6 +3,11 @@
  */
 require(['jquery'], function ($) {
     $(function () {
+        if (navigator.userAgent.indexOf("Chrome") === -1) {
+            alert('请使用谷歌浏览器打开!');
+            return;
+        }
+
         require(['toonly/javascripts/only-pageslide', 'toonly/javascripts/only-router'], function (pageSlide, router) {
             var urlSnapshot = pageSlide.init();
             router.go(urlSnapshot ? urlSnapshot : 'blogcontent/index_.html');
